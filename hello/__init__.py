6a2d608294fabd282aa445976a1351d804a7a84d
Симонова Валентина Владимировна <171043@edu.fa.ru>
2020-10-17 16:08:22 +0300

    comm

--- hello/__init__.py
+++ hello/__init__.py
@@ -14,24 +14,24 @@ verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
 
 def sample(l, n = 1):
 
-result = random.sample(l, n)
+    result = random.sample(l, n)
 
-if n == 1:
+    if n == 1:
 
-return result[0]
+        return result[0]
 
-return result
+    return result
 
 def generate_buzz():
 
-buzz_terms = sample(buzz, 2)
+    buzz_terms = sample(buzz, 2)
 
-phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
+    phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
 
-sample(verbs), buzz_terms[1]])
+    sample(verbs), buzz_terms[1]])
 
-return phrase.title()
+        return phrase.title()
 
 if __name__ == "__main__":
 
-print (generate_buzz())
+    print (generate_buzz())
