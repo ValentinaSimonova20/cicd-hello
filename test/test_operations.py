fb999d8822ba54ce88a5800b506b624bbb0b365f
ValentinaSimonova20 <simonova.valentina19@gmail.com>
2020-10-25 00:31:30 +0300

    new comm

--- test/test_operations.py
+++ test/test_operations.py
@@ -5,6 +5,9 @@ class TestArimtOp(unittest.TestCase):
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
         
+    def test_upper(self):
+        self.assertEqual('tree'.upper(), 'TREE')
+        
         
 if __name__ == '__main__':
     unittest.main()
